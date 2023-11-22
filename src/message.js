8df2c32f878a31d558608e52fdcaae500b42e2e2
Hasan TEKGÜL <hasan.tekgul@hmb.gov.tr>
2023-11-22 16:48:06 +0300

    mesaj güncelle

--- src/message.js
+++ src/message.js
@@ -3,25 +3,29 @@ import chalk from 'chalk'
 import Config from './config.js'
 
 export function warnConfigNotFound () {
-  const example = `${chalk.yellow('{')}
-  ${chalk.blue('"gitlabBaseUrl"')}: ${chalk.dim('"https://git.company.com"')},
-  ${chalk.blue('"gitlabToken"')}: ${chalk.dim('"xxx"')}
-${chalk.yellow('}')}`
   console.log(boxen(`${chalk.yellow('Uyarı:')}
 
 Gitlab ayarları bulunamadı.
 
-${chalk.blue(Config.path)} dosyası bulunamadı.
-Bu uygulamayı düzgün şekilde çalıştırabilmek için Gitlab bilgilerine ihtiyacınız var.
-Bu dosyayı oluşturup ilgili ayarları aşağıdaki gibi girebilirsiniz:
+Gitlab ayarlarını aşağıdaki şekillerde belirtebilirsiniz:
 
-${example}
+${chalk.yellow('1.')} ${chalk.blue(Config.path)} dosyası içinde. Dosya içeriği aşağıdaki şekilde olmalıdır:
+
+  ${chalk.yellow('{')}
+    ${chalk.blue('"gitlabBaseUrl"')}: ${chalk.dim('"https://git.company.com"')},
+    ${chalk.blue('"gitlabToken"')}: ${chalk.dim('"xxx"')}
+  ${chalk.yellow('}')}
 
 Bu dosyayı ilgili ayarlarla birlikte oluşturmak için;
 
-${chalk.green('grc config --url https://git.company.com --token xxx')}
+  ${chalk.green('grc config --url https://git.company.com --token xxx')}
 
 komutunu da kullanabilirsiniz.
+
+${chalk.yellow('2.')} Ortam değişkenleriyle. Aşağıdaki ortam değişkenlerini kullanabilirsiniz:
+
+  export ${chalk.blue('GITLAB_BASE_URL')}=${chalk.dim('"https://git.company.com"')}
+  export ${chalk.blue('GITLAB_TOKEN')}=${chalk.dim('"xxx"')}
 `, { padding: 1 }))
   console.log()
 }
