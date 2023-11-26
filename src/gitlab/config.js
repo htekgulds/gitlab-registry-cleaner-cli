186225adec991d64215b8e2bee158ab4c41142d5
Hasan TEKGÜL <htekgulds@gmail.com>
2023-11-26 21:31:12 +0300

    log kaldır

--- src/gitlab/config.js
+++ src/gitlab/config.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 const Gitlab = {
   client: null,
   setup (config) {
-    console.log('Setting up gitlab')
-
     this.client = axios.create({
       baseURL: config.gitlabBaseUrl + '/api/v4',
       timeout: 10000,
