02d83fef10ba894cda2de790c4b6c1350af89b6e
Hasan TEKGÜL <hasan.tekgul@hmb.gov.tr>
2023-11-27 10:49:06 +0300

    dotenv pakete ekleme

--- bin/grc.js
+++ bin/grc.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-import dotenvFlow from 'dotenv-flow'
 import main from '../src/index.js'
-import { debug } from '../src/util.js'
 
-// Get options from .env file while developing for ease of use
-debug(() => dotenvFlow.config())
-
-// const config = Config.set()
-// debug(() => console.log('Config', config))
-
-// Warn if Gitlab settings not present and exit
-// if (!Config.isValid()) {
-//   help()
-//   process.exit(1)
-// }
-
-// Gitlab.setup(config)
 main()
