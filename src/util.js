02766630bc8de40b39437b1d7d0869813e74be15
Hasan TEKGÜL <hasan.tekgul@hmb.gov.tr>
2023-11-27 16:21:19 +0300

    default'dan al

--- src/util.js
+++ src/util.js
@@ -1,7 +1,9 @@
-const GROUP_BY_REGEX = /^[a-zA-Z0-9.]+(-[0-9]+)?-(.*)$/
+// const GROUP_BY_REGEX = /^[a-zA-Z0-9.]+(-[0-9]+)?-(.*)$/
+
+import { GROUP_TAGS_REGEX } from './defaults'
 
 export function getSuffix (tag) {
-  const match = tag.match(GROUP_BY_REGEX)
+  const match = tag.match(GROUP_TAGS_REGEX)
   debug(() => console.log('Match:', match))
 
   if (match) return match[2]
