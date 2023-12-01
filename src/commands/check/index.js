0bb62bae9554634abfa393334927087db940be1b
Hasan TEKGÜL <hasan.tekgul@hmb.gov.tr>
2023-12-01 09:46:36 +0300

    log için try catch'e gerek yok

--- src/commands/check/index.js
+++ src/commands/check/index.js
@@ -1,13 +1,13 @@
 import Gitlab from '../../gitlab/config'
 import { logger } from '../../util'
 
-export default async function check (argv) {
+export default async function check () {
+  logger.warn('WARN: level 0')
+  logger.info('INFO: Level 1')
+  logger.debug('DEBUG: Level 2')
+  logger.trace('TRACE: Level 3')
   try {
     const response = await Gitlab.client.get('/version')
-    logger.warn('WARN: level 0')
-    logger.info('INFO: Level 1')
-    logger.debug('DEBUG: Level 2')
-    logger.trace('TRACE: Level 3')
     logger.warn('Gitlab Version:', response.data)
   } catch (err) {
     console.error(err)
